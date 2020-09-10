ccdbff3761dc7b6211ef74e855480c50d14c79c8
yuxino <nbsawz@gmail.com>
2020-09-10 13:58:53 +0800

    feat: modify vuepress base path

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,7 +1,7 @@
 const { description } = require("../../package");
 
 module.exports = {
-  base: "/website",
+  base: "/website/",
   /**
    * Ref：https://v1.vuepress.vuejs.org/config/#title
    */
