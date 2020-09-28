e44ea082908a98d48cc6b45e792a01002a6f795b
glitchboyl <lonelyliar8523@gmail.com>
2020-09-29 01:54:49 +0800

    docs(wip): useCounter and its demo

---
+++ docs/.vitepress/components/UseCounterDemo.vue
@@ -0,0 +1,28 @@
+
+<template>
+  <div>
+    <p>{{ count }} [min: 1; max: 10; x: 2;]</p>
+    <button style="margin-right: 6px;" @click="inc">inc</button>
+    <button style="margin-right: 6px;" @click="dec">dec</button>
+    <button style="margin-right: 6px;" @click="set(6)">set(6)</button>
+    <button @click="reset">reset</button>
+  </div>
+</template>
+
+<script lang="ts">
+import { useCounter } from "vueposu";
+
+export default {
+  setup() {
+    const [count, { inc, dec, set, reset }] = useCounter(100, { min: 1, max: 10, x: 2 });
+
+    return {
+      count,
+      inc, 
+      dec, 
+      set, 
+      reset,
+    };
+  },
+};
+</script>
--- docs/useCounter.md
+++ docs/useCounter.md
@@ -1,1 +1,16 @@
-useCounter
+# useCounter
+
+A hook that can manage the count.
+
+## Usage
+
+<script>
+import UseCounterDemo from './.vitepress/components/UseCounterDemo.vue'
+
+export default {
+    components: {
+        UseCounterDemo
+    }
+}
+</script>
+<UseCounterDemo />
